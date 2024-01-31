49c0b91aa4bc02bb803fa5515ca60bbb845333e3
Md Rasel Rana <raselges127@gmail.com>
2024-01-31 22:37:46 +0600

    chages

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function Homepage(){
-        return view('welcome');
+    public function HomePage()
+    {
+        return view('pages.home-page');
     }
 }
